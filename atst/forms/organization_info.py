7ce86344504bafd707ca93e7d66317a9fa1c0334
richard-dds <richard-ctr@friends.dds.mil>
2018-06-20 11:20:31 -0400

    WIP

--- atst/forms/organization_info.py
+++ atst/forms/organization_info.py
@@ -1,4 +1,29 @@
 from wtforms_tornado import Form
+from wtforms.fields import StringField, RadioField, FormField
+from wtforms.validators import Required
+from .date import DateForm
+
 
 class OrganizationInfoForm(Form):
-    pass
+    name = StringField('Name', validators=[Required()])
+    email = StringField('Email', validators=[Required()])
+    phone_number = StringField('Phone Number', validators=[Required()])
+    department = StringField('Office Symbol / Department', validators=[Required()])
+    citizenship = RadioField('Citizenship',
+                             validators=[Required()],
+                             choices=[
+                                 ('united_states', 'United States'),
+                                 ('foreign_national', 'Foreign National'),
+                                 ('other', 'Other')
+                             ])
+    designation = RadioField('Designation of Person',
+                             validators=[Required()],
+                             choices=[
+                                 ('military', 'Military'),
+                                 ('civilian', 'Civilian'),
+                                 ('contractor', 'Contractor')
+                             ])
+    ia_training_completion_date = FormField(
+        DateForm, 'Last IA Training Completion Date')
+    collab_name = StringField('Name', validators=[Required()])
+    collab_email = StringField('Email', validators=[Required()])
