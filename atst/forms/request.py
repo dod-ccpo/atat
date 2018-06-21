4a9e4c437a435cd9fed05fb2436c9328d9154587
richard-dds <richard-ctr@friends.dds.mil>
2018-06-21 09:57:05 -0400

    Added some new fields, but form is still WIP

--- atst/forms/request.py
+++ atst/forms/request.py
@@ -4,6 +4,8 @@ from wtforms.fields import (
     StringField,
     SelectField,
     FormField,
+    TextAreaField,
+    BooleanField
 )
 from wtforms.validators import Required, ValidationError
 from wtforms_tornado import Form
@@ -19,55 +21,20 @@ class RequestForm(Form):
         'Please estimate the number of applications that might be supported by this request',
         validators=[Required()],
     )
-    input_estimate = SelectField(
-        'How did you arrive at this estimate?',
-        validators=[Required()],
-        choices=[
-            ('', '- Select -'),
-            ('calculator', 'CSP usage calculator'),
-            ('B', 'Option B'),
-            ('C', 'Option C'),
-        ],
-    )
-    # no way to apply a label to a whole nested form like this
-    date_start = FormField(DateForm)
-    period_of_performance = SelectField(
-        'Desired period of performance',
-        validators=[Required()],
-        choices=[
-            ('', '- Select -'),
-            ('value1', '30 days'),
-            ('value2', '60 days'),
-            ('value3', '90 days'),
-        ],
-    )
-    classification_level = RadioField(
-        'Classification level',
-        validators=[Required()],
-        choices=[
-            ('unclassified', 'Unclassified'),
-            ('secret', 'Secret'),
-            ('top-secret', 'Top Secret'),
-        ],
-    )
-    primary_service_branch = StringField(
-        'Primary service branch usage', validators=[Required()]
-    )
-    cloud_model = RadioField(
-        'Cloud model service',
-        validators=[Required()],
-        choices=[('iaas', 'IaaS'), ('paas', 'PaaS'), ('both', 'Both')],
-    )
-    number_of_cores = IntegerField('Number of cores', validators=[Required()])
+    total_cores = IntegerField('Total Number of vCPU cores', validators=[Required()])
     total_ram = IntegerField('Total RAM', validators=[Required()])
-    object_storage = IntegerField('Total object storage', validators=[Required()])
-    server_storage = IntegerField('Total server storage', validators=[Required()])
-    total_active_users = IntegerField('Total active users', validators=[Required()])
-    total_peak_users = IntegerField('Total peak users', validators=[Required()])
-    total_requests = IntegerField('Total requests', validators=[Required()])
-    total_environments = IntegerField('Total environments', validators=[Required()])
+    total_object_storage = IntegerField('Total object storage', validators=[Required()])
+    total_server_storage = IntegerField('Total server storage', validators=[Required()])
 
-    # this is just an example validation; obviously this is wrong.
-    def validate_total_ram(self, field):
-        if (field.data % 2) != 0:
-            raise ValidationError("RAM must be in increments of 2.")
+    date_start = FormField(DateForm, 'Date you expect to start accessing this cloud resource', validators=[Required()])
+    app_description = TextAreaField('Please briefly describe how your team is expecting to use the JEDI Cloud')
+    service_branch = StringField(
+        'Which service branches will this resource be supporting?', validators=[Required()]
+    )
+    has_contractor_advisor = BooleanField('Do you have a contractor to advise and assist you with using cloud services?', validators=[Required()])
+    has_migration_office = BooleanField("Do you have a migration office that you're working with to migrate to the cloud?", validators=[Required()])
+    working_with = TextAreaField('Please describe who you are working with')])
+    is_existing_app = BooleanField('Is this an existing site/application?', validators=[Required()])
+
+    uiis = TextAreaField('Please provide the Unique Item Identifier (UII)s related to your request')
+    pes = TextAreaField('Please provide the Program Element (PE) Numbers related to your request')
