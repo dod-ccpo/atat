47083c80e095f320cc706432e179479654e1540b
mikepw <michaelpapetti@Michaels-MBP.fios-router.home>
2020-05-15 11:16:03 -0400

    adds convenience script to test tf plan

--- script/tf_plan.sh
+++ script/tf_plan.sh
@@ -2,7 +2,6 @@
 
 
 if [ -z "$DEPLOY_TAG" ] || [ -z "$TF_DIR" ] || [ -z "$VAULT_SECRET" ] || [ -z "$VAULT_URL" ] || [ -z "$VAULT_CLIENT_ID" ] ||  [ -z "$VAULT_TENANT" ] ||  [ -z "$SUBSCRIPTION_ID" ]
-
 then
 echo "Set DEPLOY_TAG, TF_DIR, VAULT_SECRET, VAULT_CLIENT_ID, VAULT_TENANT, SUBSCRIPTION_ID"
 exit 1;
@@ -14,6 +13,7 @@ cd $TF_DIR && terraform show $TF_DIR/plan.tfplan
 
 
 terraform show $TF_DIR/plan.tfplan
+
 
 
 
