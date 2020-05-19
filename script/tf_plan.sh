25abadbecd68777bfa29e212fabeb32d73c5a6f0
mikepw <michaelpapetti@ip-192-168-1-153.us-gov-west-1.compute.internal>
2020-05-19 15:01:44 -0400

    fixes spacing err, changes dir

--- script/tf_plan.sh
+++ script/tf_plan.sh
@@ -2,8 +2,8 @@
 
 
 
-
 if [ -z "$DEPLOY_TAG" ] || [ -z "$TF_DIR" ] || [ -z "$VAULT_SECRET" ] || [ -z "$VAULT_URL" ] || [ -z "$VAULT_CLIENT_ID" ] ||  [ -z "$VAULT_TENANT" ] ||  [ -z "$SUBSCRIPTION_ID" ]
+
 
 
 then
@@ -13,6 +13,7 @@ fi
 
 cd $TF_DIR && terraform init
 cd ../../../ansible
+
 poetry run ansible-playbook ../ansible/site.yml --extra-vars "provision_pwdev=true deploy_tag=$DEPLOY_TAG tf_dir='$TF_DIR' vault_url='$VAULT_URL' vault_secret='$VAULT_SECRET' vault_client_id='$VAULT_CLIENT_ID' vault_tenant='$VAULT_TENANT' vault_subscription_id='$SUBSCRIPTION_ID'"
 cd $TF_DIR && terraform show $TF_DIR/plan.tfplan
 
