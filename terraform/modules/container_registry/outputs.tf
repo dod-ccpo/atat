e31f09522adb002ad8f6244714acb1e21ec2ce97
mikepw <mike@opsulent.com>
2020-09-17 08:26:55 -0400

    adds ansible plays for calling circle ci, adds azure firewall, breaks out tasks into files for several ansible roles

--- terraform/modules/container_registry/outputs.tf
+++ terraform/modules/container_registry/outputs.tf
@@ -2,3 +2,7 @@
 output "container_registry_name" {
   value = azurerm_container_registry.acr.name
 }
+
+ output "ops_container_registry_name" {
+   value = data.azurerm_container_registry.ops.name
+ }
