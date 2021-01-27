9823ee37193cbb4e03d66f6a11127cdd1decbc6d
 Jesus Gonzalez <jgonzalez@ddreams.co>
2021-01-27 17:51:58 -0500

    ENV/jes-dev making DEV_DEBUG_TOOL_SECRET_KEY optional | but please set your own key before run

--- atat/app.py
+++ atat/app.py
@@ -397,5 +397,5 @@ def debug_tools_bar(app):
     # the toolbar is only enabled in debug mode:
     app.debug = True
     # set a 'SECRET_KEY' to enable the Flask session cookies
-    app.config['SECRET_KEY'] = app.config["DEV_DEBUG_TOOL_SECRET_KEY"]
+    app.config['SECRET_KEY'] = app.config["DEV_DEBUG_TOOL_SECRET_KEY"] or 'jfhdfjshkjskjdjdhhe'
     toolbar = DebugToolbarExtension(app)
