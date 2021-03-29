e1474b94d3ceb541787761519a70bf8d06c1a0fe
Devonte Gabriel <devonte.gabriel-ctr@ccpo.mil>
2021-03-29 10:24:09 -0600

    uncommented function

--- atat/domain/reports.py
+++ atat/domain/reports.py
@@ -1,12 +1,11 @@
 # import pendulum
+# from flask import current_app
 
 # from atat.domain.csp.cloud.models import (
-#     CostManagementQueryCSPPayload,
-#     CostManagementQueryCSPResult,
+# CostManagementQueryCSPPayload,
+# CostManagementQueryCSPResult,
 # )
-# from atat.domain.csp.reports import prepare_azure_reporting_data
-
-# from flask import current_app
+from atat.domain.csp.reports import prepare_azure_reporting_data
 
 
 class Reports:
@@ -16,22 +15,21 @@ class Reports:
             task_order for task_order in portfolio.task_orders if task_order.is_expired
         ]
 
-    # @classmethod
-    # this function is commented out because it calls a third party api we don't have access to
-    # def get_portfolio_spending(cls, portfolio):
-    # TODO: Extend this function to make from_date and to_date configurable
+    @classmethod
+    def get_portfolio_spending(cls, portfolio):
+        # TODO: Extend this function to make from_date and to_date configurable
+        # from_date = (
+        #     pendulum.now(tz="UTC").subtract(years=1).add(days=1).format("YYYY-MM-DD")
+        # )
+        # to_date = pendulum.now(tz="UTC").format("YYYY-MM-DD")
+        rows = []
+        # if portfolio.is_provisioned:
+        #     # payload = CostManagementQueryCSPPayload(
+        #     #     from_date=from_date, to_date=to_date, **portfolio.csp_data
+        #     # )
+        #     # response: CostManagementQueryCSPResult = current_app.csp.cloud.get_reporting_data(
+        #     #     payload
+        #     # )
+        #     rows = response.properties.rows
 
-    # from_date = (
-    #     pendulum.now(tz="UTC").subtract(years=1).add(days=1).format("YYYY-MM-DD")
-    # )
-    # to_date = pendulum.now(tz="UTC").format("YYYY-MM-DD")
-    # rows = []
-    # if portfolio.is_provisioned:
-    #     # payload = CostManagementQueryCSPPayload(
-    #     #     from_date=from_date, to_date=to_date, **portfolio.csp_data
-    #     # )
-    #     # response: CostManagementQueryCSPResult = current_app.csp.cloud.get_reporting_data(
-    #     #     payload
-    #     # )
-    #     rows = response.properties.rows
-    # return prepare_azure_reporting_data(rows)
+        return prepare_azure_reporting_data(rows)
