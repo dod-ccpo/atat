5cd9d9497199cf93e3902ce6bf8c067551535095
Devonte Gabriel <devonte.gabriel-ctr@ccpo.mil>
2021-03-29 09:43:45 -0600

    sonar cloud check failed due to unused variables, commented them out

--- atat/domain/reports.py
+++ atat/domain/reports.py
@@ -1,10 +1,10 @@
 import pendulum
-from flask import current_app
+# from flask import current_app
 
-from atat.domain.csp.cloud.models import (
-    CostManagementQueryCSPPayload,
-    CostManagementQueryCSPResult,
-)
+# from atat.domain.csp.cloud.models import (
+#     CostManagementQueryCSPPayload,
+#     CostManagementQueryCSPResult,
+# )
 from atat.domain.csp.reports import prepare_azure_reporting_data
 
 
@@ -16,13 +16,15 @@ class Reports:
         ]
 
     @classmethod
-    def get_portfolio_spending(cls, portfolio):
+    # this function is commented out because it calls a third party api we don't have access to
+    # def get_portfolio_spending(cls, portfolio):
         # TODO: Extend this function to make from_date and to_date configurable
-        from_date = (
-            pendulum.now(tz="UTC").subtract(years=1).add(days=1).format("YYYY-MM-DD")
-        )
-        to_date = pendulum.now(tz="UTC").format("YYYY-MM-DD")
-        rows = []
+        
+        # from_date = (
+        #     pendulum.now(tz="UTC").subtract(years=1).add(days=1).format("YYYY-MM-DD")
+        # )
+        # to_date = pendulum.now(tz="UTC").format("YYYY-MM-DD")
+        # rows = []
         # if portfolio.is_provisioned:
         #     # payload = CostManagementQueryCSPPayload(
         #     #     from_date=from_date, to_date=to_date, **portfolio.csp_data
@@ -32,4 +34,4 @@ class Reports:
         #     # )
         #     rows = response.properties.rows
 
-        return prepare_azure_reporting_data(rows)
+        # return prepare_azure_reporting_data(rows)
