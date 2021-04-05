980e6644737420a0e6a60ae9a61240e12b7cec37
ehoward-ccpo <edward.howard-ctr@ccpo.mil>
2021-04-05 17:32:11 -0400

    AT-6113-upating-login-tests

--- uitests/framework/test_cases/test_login.py
+++ uitests/framework/test_cases/test_login.py
@@ -2,9 +2,9 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from uitests.framework.page_objects.common_methods import JediCommonMethods
-from uitests.framework.page_objects.login_page import Login
-from uitests.framework.utilities.read_properties import ReadConfig
+from page_objects.common_methods import JediCommonMethods
+from page_objects.login_page import Login
+from utilities.read_properties import ReadConfig
 
 
 class TestLogin:
@@ -12,23 +12,31 @@ class TestLogin:
 
     def test_user_login(self, setup):
         self.driver = setup
-        # self.driver.execute_script(
-        #     'browserstack_executor: {"action": "setSessionName", '
-        #     '"arguments": {"name": "FAILED User Login"}}'
-        # )
+        self.driver.execute_script(
+            'browserstack_executor: {"action": "setSessionName", '
+            '"arguments": {"name": "1. Verification of User Login"}}'
+        )
         self.driver.get(self.url2)
         self.driver.maximize_window()
-        assert "ATAT" in self.driver.page_source
+        WebDriverWait(self.driver, 30).until(
+            EC.text_to_be_present_in_element(
+                (By.CSS_SELECTOR, "a.topbar__link span.topbar__link-label"), "ATAT"
+            )
+        )
         self.login = Login(self.driver)
         self.login.userName()
         self.cm = JediCommonMethods(self.driver)
         self.cm.click_Home()
+        WebDriverWait(self.driver, 30).until(
+            EC.text_to_be_present_in_element(
+                (By.CSS_SELECTOR, "div.home__content"), "JEDI Cloud Services"
+            )
+        )
         self.login.clickLogout()
-        assert "Access the JEDI Cloud" in self.driver.page_source
         try:
             WebDriverWait(self.driver, 5).until(
                 EC.text_to_be_present_in_element(
-                    (By.CSS_SELECTOR, "h3.usa-alert-heading"), "Loggeddddout"
+                    (By.CSS_SELECTOR, "h3.usa-alert-heading"), "Logged out"
                 )
             )
             self.driver.execute_script(
