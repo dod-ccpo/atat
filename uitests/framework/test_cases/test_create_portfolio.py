f0006ec7654ebd6a4d8521a93968793f739b2c58
ehoward-ccpo <edward.howard-ctr@ccpo.mil>
2021-04-15 18:06:07 -0400

    feature/AT-6159 removed timesleep and updated validation for Create Portfolio

--- uitests/framework/test_cases/test_create_portfolio.py
+++ uitests/framework/test_cases/test_create_portfolio.py
@@ -1,6 +1,5 @@
 import random
 import string
-import time
 import pytest
 
 from selenium.common.exceptions import TimeoutException
@@ -9,6 +8,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from uitests.framework.utilities.read_properties import ReadConfig
 from uitests.framework.page_objects.new_portfolio_page import AddNewPortfolioPages
+from uitests.framework.page_objects import PageObjectMethods
 
 
 @pytest.mark.smoke
@@ -16,24 +16,29 @@ class TestCreatePortfolio:
     url2 = ReadConfig.getLoginLocalURL()
 
     def test_create_portfolio(self, setup):
+        # Setting up driver, session/test name, maximizing window
         self.driver = setup
+        self.driver.get(self.url2)
+        self.driver.maximize_window()
+        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
         self.driver.execute_script(
             'browserstack_executor: {"action": "setSessionName", '
             '"arguments": {"name": "3. Create Portfolio"}}'
         )
-        self.driver.get(self.url2)
-        self.driver.maximize_window()
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-        assert "ATAT" in self.driver.page_source
+        # Initializing Page Objects
         self.port = AddNewPortfolioPages(self.driver)
+        self.cm = PageObjectMethods(self.driver)
+
+        # Generating random portfolio name
+        self.pName = "Test Portfolio" + random_generator()
+
+        self.cm.validate_atat()
+        self.cm.validate_jedi()
         self.port.click_new_portfolio()
         self.port.validate_new_portfolio()
         self.port.validate_name_desc()
-        self.pName = "Test Portfolio" + random_generator()
         self.port.enter_portfolio_name(self.pName)
-        self.port.enter_portfolio_description(
-            "Entering the description to verify the text"
-        )
+        self.port.enter_portfolio_description("Entering the description to verify the text")
         self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
         self.port.select_checkbox()
         self.port.click_save_portfolio_btn()
@@ -53,7 +58,7 @@ class TestCreatePortfolio:
                 'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": '
                 '"Text is NOT Matched"}}'
             )
-        print("Task Orders")
+        print("Test: Create Portfolio")
         self.driver.quit()
 
 
