f55203374675fda6a3e728e2d1a6c57755cc4ab7
Jesus Gonzalez <jesus.gonzalez-ctr@ccpo.mil>
2021-04-19 23:15:15 -0400

    Now it is working as expected. both manage now are coming from the same variable on the YAML file called translation that is the standard for the ATAT app. instead of the hard code text as it was before.

--- atat/forms/validators.py
+++ atat/forms/validators.py
@@ -99,11 +99,12 @@ def file_length(max_length=50000000, message=None):
 
 
 def alpha_numeric_name_pdf(
-    message=translate("forms.validators.alpha_numeric_message")
-    + " and File name must end on '.pdf'.",
+    message=translate("forms.task_order.filename_error"),
 ):
     return Regexp(
-        regex=r"^[A-Za-z0-9\-_ \.\/\\]*\.pdf$", message=message, flags=re.IGNORECASE
+        regex=r"^([A-Za-z0-9\-_ \.\/\\]*(\.pdf))?$",
+        message=message,
+        flags=re.IGNORECASE,
     )
 
 
