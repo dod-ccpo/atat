fb32f09cd6a6f13503a265197d6fc8d7d37c133b
Jesus Gonzalez (Hunter Strategy) <77741090+jGonzalez-CCPO@users.noreply.github.com>
2021-04-20 13:16:12 -0400

    AT-6121 adding subjected changes by Kyle

--- js/lib/input_validations.js
+++ js/lib/input_validations.js
@@ -139,7 +139,7 @@ export default {
   },
   restrictedFileName: {
     mask: false,
-    match: /^[A-Za-z0-9\-_ \.\/\\]*\.pdf$/i,
+    match: /^[A-Za-z0-9\-_ \.\/\\]+\.pdf$/i,
     unmask: [],
     validationError:
       'File names must end in .pdf and can only contain the characters A-Z, 0-9, space, hyphen, underscore, and period.',
