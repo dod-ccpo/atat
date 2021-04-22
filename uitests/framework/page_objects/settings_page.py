42af9facd01b8a86863f984f0733d80f7e3cbf83
ehoward-ccpo <edward.howard-ctr@ccpo.mil>
2021-04-22 04:07:42 -0400

    feature/AT-6163 removed unused functions, elements, and methods

--- uitests/framework/page_objects/settings_page.py
+++ uitests/framework/page_objects/settings_page.py
@@ -2,11 +2,6 @@ class SettingsPages:
     btn_select_portfolio_css = ".sidenav__link-label"
     btn_settings_css = "span.icon.icon--cog"
 
-    # txt_portfolio_name_css = "#name"
-    # txt_portfolio_description_css = "#description"
-    # btn_save_changes_css = "#app-root > div.global-layout > div.global-panel-container > div > div > div.portfolio-content > div.portfolio-admin > section > form > div.edit-portfolio-name.action-group > button"
-    # btn_add_portfolio_manager_css = "#app-root > div.global-layout > div.global-panel-container > div > div > div.portfolio-content > div.portfolio-admin > div > a"
-
     def __init__(self, driver):
         self.driver = driver
 
@@ -15,19 +10,3 @@ class SettingsPages:
 
     def click_settings(self):
         self.driver.find_element_by_css_selector(self.btn_settings_css).click()
-
-    # #NOT USED - REMOVE
-    # def edit_portfolio_name(self):
-    #     self.driver.find_element_by_css(self.txt_portfolio_name_css).send_keys()
-    
-    # #NOT USED - REMOVE
-    # def edit_portfolio_description(self):
-    #     self.driver.find_element_by_css(self.txt_portfolio_description_css).send_keys()
-
-    # #NOT USED - REMOVE
-    # def click_save_button(self):
-    #     self.driver.find_element_by_css(self.btn_save_changes_css).click()
-
-    # #NOT USED - REMOVE
-    # def click_add_portfolio_manager(self):
-    #     self.driver.find_element_by_css(self.btn_add_portfolio_manager_css).click()
