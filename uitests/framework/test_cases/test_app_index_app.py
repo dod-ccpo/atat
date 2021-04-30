5be4b63479e1a6e2615a6340233d1065d83f56df
palaparthi-ccpo <prasanthi.alaparthi-ctr@ccpo.mil>
2021-04-30 15:40:40 -0400

    feature/AT-6197-Relocate_generators-to-POs-removed the generators from the file and referenced from POs

--- uitests/framework/test_cases/test_app_index_app.py
+++ uitests/framework/test_cases/test_app_index_app.py
@@ -1,5 +1,3 @@
-import string
-import random
 import pytest
 
 from selenium.webdriver.common.by import By
@@ -7,7 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from uitests.framework.utilities.read_properties import ReadConfig
-from uitests.framework.page_objects.new_portfolio_page import AddNewPortfolioPages
+from uitests.framework.page_objects.new_portfolio_page import AddNewPortfolioPages, random_generator
+from uitests.framework.page_objects.task_order_page import random_no_generator
 from uitests.framework.page_objects.application_page import CreateApplicationPages
 from uitests.framework.page_objects import PageObjectMethods
 
@@ -50,7 +49,8 @@ class TestApplicationWithIndex:
         self.port.enter_portfolio_description(
             "Entering the description to verify the text"
         )
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.port.select_checkbox()
         self.port.click_save_portfolio_btn()
         self.app.click_applications()
@@ -62,7 +62,8 @@ class TestApplicationWithIndex:
         self.app.validate_app_save()
         self.app.click_next_add_members()
         self.app.validate_add_members()
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.app.click_add_member()
         self.app.enter_first_name("Brandon")
         self.app.enter_last_name("Buchannan")
@@ -77,7 +78,8 @@ class TestApplicationWithIndex:
         self.driver.execute_script(
             "document.querySelector('#environment_roles-1-role-None').value='BILLING_READ'"
         )
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.app.click_save_app()
         self.app.click_save_app_next()
         self.app.click_applications()
@@ -107,11 +109,3 @@ class TestApplicationWithIndex:
             )
         print("Test: Verification of Application Indexing")
         self.driver.quit()
-
-
-def random_generator(size=15, chars=string.ascii_lowercase + string.digits):
-    return "".join(random.choice(chars) for x in range(size))
-
-
-def random_no_generator(size=17, chars=string.digits):
-    return "".join(random.choice(chars) for x in range(size))
