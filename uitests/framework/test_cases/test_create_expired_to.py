a114af74f453680453af7fd11d6794dbe18b968d
palaparthi-ccpo <prasanthi.alaparthi-ctr@ccpo.mil>
2021-04-30 13:17:08 -0400

    feature/AT-6197-Relocate_generators-to-POs-removed the genarators from  file and referenced from related POs file

--- uitests/framework/test_cases/test_create_expired_to.py
+++ uitests/framework/test_cases/test_create_expired_to.py
@@ -1,12 +1,16 @@
 import os
 import time
-import random
-import string
 import pytest
 
 from selenium.common.exceptions import TimeoutException
-from uitests.framework.page_objects.new_portfolio_page import AddNewPortfolioPages
-from uitests.framework.page_objects.task_order_page import TaskOrderPage
+from uitests.framework.page_objects.new_portfolio_page import (
+    AddNewPortfolioPages,
+    random_generator,
+)
+from uitests.framework.page_objects.task_order_page import (
+    TaskOrderPage,
+    random_no_generator,
+)
 from uitests.framework.utilities.read_properties import ReadConfig
 from uitests.framework.page_objects import PageObjectMethods
 
@@ -25,19 +29,22 @@ class TestCreateExpiredTaskOrder:
         )
         self.driver.get(self.url2)
         self.driver.maximize_window()
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.cm = PageObjectMethods(self.driver)
         self.cm.validate_atat()
         self.port = AddNewPortfolioPages(self.driver)
         self.port.click_new_portfolio()
         self.port.validate_new_portfolio()
         self.port.validate_name_desc()
+        # Random Generator for unique Porfolio Name
         self.pName = "Test Portfolio" + random_generator()
         self.port.enter_portfolio_name(self.pName)
         self.port.enter_portfolio_description(
             "Entering the description to verify the text"
         )
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.port.select_checkbox()
         time.sleep(5)
         self.port.click_save_portfolio_btn()
@@ -48,9 +55,11 @@ class TestCreateExpiredTaskOrder:
         self.to = TaskOrderPage(self.driver)
         self.to.click_task_order()
         self.to.validate_add_to()
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.to.click_add_new_to()
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.driver.execute_script(
             "document.querySelector('#pdf').style.visibility = 'visible'"
         )
@@ -62,11 +71,14 @@ class TestCreateExpiredTaskOrder:
         file_input.send_keys(absolute_file_path)
         self.to.click_next_add_TO_number()
         time.sleep(10)
+        #Random Generator for Unique TO#
         self.tnumber = random_no_generator()
         self.to.enter_TO_number(self.tnumber)
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.to.click_next_add_clin_number()
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.to.enter_clin_number("0012")
         self.to.add_clin_value("800,000")
         self.to.add_obligated_clin_value("100,000")
@@ -78,7 +90,8 @@ class TestCreateExpiredTaskOrder:
         self.to.add_end_year("2021")
         self.to.click_next_review_TO()
         self.to.click_confirm()
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.driver.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
         self.to.click_checkbox_one()
         self.to.click_check_box_two()
         self.to.click_submit_TO()
@@ -100,11 +113,3 @@ class TestCreateExpiredTaskOrder:
             )
 
             self.driver.quit()
-
-
-def random_generator(size=15, chars=string.ascii_lowercase + string.digits):
-    return "".join(random.choice(chars) for x in range(size))
-
-
-def random_no_generator(size=17, chars=string.digits):
-    return "".join(random.choice(chars) for x in range(size))
