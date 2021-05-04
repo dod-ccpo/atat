db8a0d9255372436508fe5c3aec05a647e9c12bb
Jason Burkert (Hunter Strategy) <77642966+jasonburkert-ccpo@users.noreply.github.com>
2021-05-04 16:57:26 -0400

    Remove dev dependencies 'ipdb' and 'ipython' (#2314)
    * remove dev packages ipdb and ipython
    
    * remove script/console due to removal of ipython
    
    * remove ipython_setup.py

--- ipython_setup.py
+++
@@ -1,9 +0,0 @@
-from atat.app import make_config, make_app
-
-app = make_app(make_config())
-ctx = app.app_context()
-ctx.push()
-
-print(
-    "\nWelcome to atat. This shell has all models in scope, and a SQLAlchemy session called db."
-)
